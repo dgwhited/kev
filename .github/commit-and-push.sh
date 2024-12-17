73ecf306df883bf4c8e11da466bed5d1c79bf89a
Dustin Whited <dgwhited@Dustins-MacBook-Pro.local>
2024-12-17 15:46:02 -0600

    init

---
+++ .github/commit-and-push.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+CHANGES=0
+
+echo "Commit and Push"
+echo "------------------"
+echo "Current Hash: ${GITHUB_SHA}"
+
+# Git creds
+git config --global user.email "${EMAIL}"
+git config --global user.name "${NAME}"
+
+echo "git add -A known_exploited_vulnerabilities.json"
+git add -A known_exploited_vulnerabilities.json
+
+# Test if changes
+git diff HEAD --quiet runbooks || CHANGES=1
+
+# If changes are present
+if [[ ${CHANGES} -eq 1 ]]; then
+
+  # Add and commit
+
+
+  git commit -m 'New KEVs Detected'
+  
+  # Create date var for release name
+  TODAY="$(date +'%Y-%m-%d')"
+
+  # Push tag
+  echo "git push--set-upstream origin main"
+  git push --set-upstream origin main
+
+else
+  echo "No changes detected"
+fi
